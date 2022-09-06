5027b24915b1e0e3370970cc4231ce36899c60d7
LucianoPierdona <lucianompj1@gmail.com>
2022-09-05 23:47:30 -0300

    add env variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 import { runSeed } from "./fixtures/run-seed";
 import router from "./routes";
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -11,14 +12,10 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use(router);
 
-mongoose.connect(
-  "mongodb+srv://testlets:txpvxGveHklUUXdY@cluster0.37fbyz5.mongodb.net/movies?retryWrites=true&w=majority",
-  {},
-  () => {
-    console.log("connected to database");
-    runSeed();
-  }
-);
+mongoose.connect(process.env.MONGODB_URL!, {}, () => {
+  console.log("connected to database");
+  runSeed();
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
