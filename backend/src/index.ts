dabc2c77a3790082e51d9c9ae692c3b3036f7db5
LucianoPierdona <lucianompj1@gmail.com>
2022-09-05 22:00:44 -0300

    add backend initial files

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
