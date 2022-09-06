18aa8f73c928ee9b88103c4add7a2c510d62e5a5
LucianoPierdona <lucianompj1@gmail.com>
2022-09-06 09:46:21 -0300

    update useAdmin to include missing methods

--- frontend/src/views/admin/use-admin.ts
+++ frontend/src/views/admin/use-admin.ts
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { User } from '../shared/modules/user/model';
-import { getUsers } from '../shared/modules/user/api/get-users';
+import { User } from "../shared/modules/user/model";
+import { getUsers } from "../shared/modules/user/api/get-users";
 
 export interface HookData {
   users: User[];
@@ -10,12 +10,13 @@ export interface HookData {
 const useAdmin = (): HookData => {
   const [users, setUsers] = useState<User[]>([]);
 
-  // FILL IN THE GAPS
-  // Load users ...
+  useEffect(() => {
+    getUsers().then((res) => setUsers(res));
+  }, []);
 
   return {
     users,
-  }
+  };
 };
 
 export default useAdmin;
