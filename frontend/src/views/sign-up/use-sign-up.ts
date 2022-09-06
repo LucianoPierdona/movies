a57a07563eed03b271cab1e5eee93d17506acd0e
LucianoPierdona <lucianompj1@gmail.com>
2022-09-06 09:44:33 -0300

    update useSignUp hook to include missing methods

--- frontend/src/views/sign-up/use-sign-up.ts
+++ frontend/src/views/sign-up/use-sign-up.ts
@@ -1,11 +1,11 @@
-import { useState, useEffect, useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { User } from '../shared/modules/user/model';
-import { Movie } from '../shared/modules/movie/model';
-import { addUser } from '../shared/modules/user/api/add-user';
-import { getMovies } from '../shared/modules/movie/api/get-movies';
-import { range } from '../shared/utils/common';
+import { User } from "../shared/modules/user/model";
+import { Movie } from "../shared/modules/movie/model";
+import { addUser } from "../shared/modules/user/api/add-user";
+import { range } from "../shared/utils/common";
+import { getMovies } from "../shared/modules/movie/api/get-movies";
 
 export interface HookData {
   data: User;
@@ -25,11 +25,11 @@ const useSignUp = (): HookData => {
   const navigate = useNavigate();
 
   const [data, setData] = useState<User>({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    avatarBase64: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    avatarBase64: "",
     movie: undefined,
     sitRow: undefined,
     sitPlace: undefined,
@@ -37,6 +37,10 @@ const useSignUp = (): HookData => {
 
   const [movies, setMovies] = useState<Movie[]>([]);
   const [movieId, setMovieId] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    getMovies().then((res) => setMovies(res));
+  }, []);
 
   const sitRows = useMemo(() => {
     return range(1, 15);
@@ -46,9 +50,62 @@ const useSignUp = (): HookData => {
     return range(1, 45);
   }, []);
 
-  // FILL IN THE GAPS
-  // Handlers go here
-  // ...
+  const handleChangeMovieId = (movieId: string) => {
+    setMovieId(movieId);
+
+    const movie = movies.find((movie) => movie._id === movieId);
+
+    setData((data) => ({
+      ...data,
+      movie,
+    }));
+  };
+
+  const handleChangeSitRow = (value: string | undefined) => {
+    setData((data) => ({
+      ...data,
+      sitRow: value ? parseInt(value) : undefined,
+    }));
+  };
+
+  const handleChangeSitPlace = (value: string | undefined) => {
+    setData((data) => ({
+      ...data,
+      sitPlace: value ? parseInt(value) : undefined,
+    }));
+  };
+
+  const handleChangeData = (fieldName: string, value: string) => {
+    setData((data) => ({
+      ...data,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleChangeAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    if (e.target.files) {
+      const reader = new FileReader();
+
+      reader.addEventListener(
+        "load",
+        () => {
+          const res = reader.result;
+
+          if (res && typeof res === "string") {
+            setData((v) => ({
+              ...v,
+              avatarBase64: res,
+            }));
+          }
+        },
+        false
+      );
+
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
 
   const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>
@@ -57,13 +114,13 @@ const useSignUp = (): HookData => {
 
     await addUser(data);
 
-    navigate('/success');
+    navigate("/success");
   };
 
   const handleClearAvatar = (): void => {
     setData((v) => ({
       ...v,
-      avatarBase64: '',
+      avatarBase64: "",
     }));
   };
 
@@ -79,7 +136,7 @@ const useSignUp = (): HookData => {
     handleChangeMovieId,
     handleChangeSitRow,
     handleChangeSitPlace,
-  }
+  };
 };
 
 export default useSignUp;
