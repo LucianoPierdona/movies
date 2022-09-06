fe57745822903b7b822185609a287151d7afc7a3
LucianoPierdona <lucianompj1@gmail.com>
2022-09-05 22:14:59 -0300

    add routes for register

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { registerRouter } from "./register";
 import { moviesRouter } from "./movies";
 
 const router = Router();
 
 router.use("/movies", moviesRouter);
+router.use("/auth", registerRouter);
 
 export default router;
