4c1355ef6c5e14d003b2fb523fecb176da219428
LucianoPierdona <lucianompj1@gmail.com>
2022-09-05 22:10:50 -0300

    add routes for movies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import router from "./routes";
 
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
