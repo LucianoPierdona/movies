c91ce283bbf4867f422ffb90c731e68d308e970f
LucianoPierdona <lucianompj1@gmail.com>
2022-09-06 10:40:18 -0300

    add support to cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { runSeed } from "./fixtures/run-seed";
 import router from "./routes";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 
